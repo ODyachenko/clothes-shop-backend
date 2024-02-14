161e68dd1225c0c656de5afcdba68d8862017431
ODyachenko <oleg1696@gmail.com>
2024-02-14 11:36:07 +0200

    edit users model

--- users/models.py
+++ users/models.py
@@ -2,5 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-# class User(AbstractUser):
-#     image = models.ImageField(upload_to='users_avatars', null=True, blank=True)
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_avatars', null=True, blank=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
