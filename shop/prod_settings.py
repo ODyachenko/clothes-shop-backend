39ee69d7ed7d35675202cff67b730c06bea60472
ODyachenko <oleg1696@gmail.com>
2024-03-11 11:00:54 +0200

    delete mysql config

--- shop/prod_settings.py
+++ shop/prod_settings.py
@@ -1,6 +1,5 @@
 import environ
 import os
-import dj_database_url
 
 env = environ.Env()
 
@@ -29,11 +28,6 @@ DATABASES = {
         }  
     }  
 }
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
-del DATABASES['default']['OPTIONS']['sslmode'] 
-DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
